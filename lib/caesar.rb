b55cd8dda4258c80c97dbe73bc9a27c48a4e1ace
Aaron Contreras <aaronlcaq@gmail.com>
2020-07-16 13:19:13 -0500

    Refactor with tests

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,23 +1,36 @@
+# frozen_string_literal: true
+
 def shift(base_value, range, ascii_letter, shift_factor)
   raw_shifts = ascii_letter - base_value + shift_factor
   net_shifts = (raw_shifts % range + range) % range
-  shifted_letter = (base_value + net_shifts).chr
+  (base_value + net_shifts).chr
+end
+
+def character_types
+  {
+    uppercase: /[A-Z]/,
+    lowercase: /[a-z]/,
+    digit: /[0-9]/
+  }
+end
+
+def shifted_character(letter, shift_factor)
+  ascii_letter = letter.ord
+
+  if letter.match? character_types[:uppercase]
+    shift(65, 26, ascii_letter, shift_factor)
+  elsif letter.match? character_types[:lowercase]
+    shift(97, 26, ascii_letter, shift_factor)
+  elsif letter.match? character_types[:digit]
+    shift(48, 10, ascii_letter, shift_factor)
+  else
+    letter
+  end
 end
 
 def caesar_cipher(string, shift_factor)
-  uppercase = /[A-Z]/
-  lowercase = /[a-z]/
-  digit = /[0-9]/
   letters = string.chars
-  shifted_string = letters.map do |letter|
-    if letter.match? uppercase
-      shift(65, 26, letter.ord, shift_factor)
-    elsif letter.match? lowercase
-      shift(97, 26, letter.ord, shift_factor)
-    elsif letter.match? digit
-      number = shift(48, 10, letter.ord, shift_factor)
-    else
-      letter
-    end
+  letters.map do |letter|
+    shifted_character(letter, shift_factor)
   end.join
 end
