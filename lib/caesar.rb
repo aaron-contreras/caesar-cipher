30fa105d150f0e77b3d25fc67330080f0c4ee5a2
Aaron Contreras <aaronlcaq@gmail.com>
2020-07-16 12:43:24 -0500

    Move the user interactivity to its own file

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -21,30 +21,3 @@ def caesar_cipher(string, shift_factor)
     end
   end.join
 end
-
-#########################
-
-puts
-puts "YOUR FRIENDLY NEIGHBORHOOD CAESAR CIPHER.".center(80)
-puts
-cipher_again = true
-user_response = nil
-
-while cipher_again
-  print "Enter some text you would like to cipher: "
-  string_to_cipher = gets.chomp
-  print "What key would you like to use to cipher it? "
-  shift_factor = gets.chomp.to_i
-  puts
-  ciphered_text = caesar_cipher(string_to_cipher, shift_factor);
-  puts "Here you go ;) -----> #{ciphered_text}"
-  puts "Make sure your key doesn't fall into the wrong hands."
-  puts
-  until user_response == 'yes' || user_response == 'no' 
-    puts "Would you like to cipher again? Yes/No"
-    user_response = gets.chomp.downcase
-  end
-
-  break puts "\nHave a good one!" if user_response == 'no'
-  user_response = nil
-end
